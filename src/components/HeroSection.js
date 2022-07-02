41f37785d32fba9903ea4630d11d60c2dbe2d3c6
AlinSterie <alinsterie@ymail.com>
2022-07-03 02:00:01 +0300

    prettier errors resolved - ready for a new build 1

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import HeroImg from '../assets/images/hero.png';
 import Button from './Button';
-import SocialMediaArrow from '../assets/images/social-media-arrow.svg';
-import ScrollDownArrow from '../assets/images/scroll-down-arrow.svg';
+// import SocialMediaArrow from '../assets/images/social-media-arrow.svg';
+// import ScrollDownArrow from '../assets/images/scroll-down-arrow.svg';
 import PText from './PText';
 
 const HeroStyles = styled.div`
